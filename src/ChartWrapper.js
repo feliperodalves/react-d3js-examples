c5ace4445ebec7ffdfb807a8d1abff4769007726
Felipe Alves <felipe.rod.alves@gmail.com>
2020-12-01 09:41:40 -0300

    001 - First Exmaple

---
+++ src/ChartWrapper.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useRef, useState } from 'react';
+import D3Chart from './D3Chart';
+
+const ChartWrapper = () => {
+  const chartRef = useRef(null);
+  const [chart, setChart] = useState(null);
+
+  useEffect(() => {
+    if (!chart) {
+      setChart(new D3Chart(chartRef.current));
+    } else {
+      chart.update();
+    }
+  }, [chart]);
+
+  return <div ref={chartRef}></div>;
+};
+
+export default ChartWrapper;
